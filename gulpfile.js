3c5b41ebb8cacafe3d2cb837b316a8481d93916c
Dennis Acosta <email@dennisacosta.me>
2024-03-23 14:07:58 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+gulp.task("serve", function () {
+  browserSync.init({
+    server: "./",
+  });
+
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.series("serve"));
